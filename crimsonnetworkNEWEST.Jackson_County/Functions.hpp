b84c5d5274dc455ee9f2bad184f18819a63051c8
Unknown <brentlucas26@yahoo.com>
2017-09-26 02:19:40 +0100

    FizDyn

--- crimsonnetworkNEWEST.Jackson_County/Functions.hpp
+++ crimsonnetworkNEWEST.Jackson_County/Functions.hpp
@@ -88,7 +88,6 @@ class Life_Client_Core {
         class LoadIntoListbox {};
         class ForcePrice {};
         class DYNMARKET_getPrice {};
-		class DYNMARKET_getIcon {};
     };
 
     class Admin {
